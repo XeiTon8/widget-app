a5616325fbd0224541332a9c25e7e5a52fdbea58
Vladyslav <vladyslavkhorunzhyi@gmail.com>
2025-04-08 14:52:35 +0300

    FIX: route path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/widget-app",
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
